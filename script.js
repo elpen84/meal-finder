4933c9c5e78a7a3176191af65f7aab6e1cc125d4
elpen84 <elplist@gmail.com>
2020-09-04 23:55:17 -0700

    css update
    styling update

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ function searchMeal(e) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
 
         if (data.meals === null) {
